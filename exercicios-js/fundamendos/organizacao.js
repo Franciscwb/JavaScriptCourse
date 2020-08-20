33944fcff8c3ff5643b89b0fe21a87c339b28ba2
franciscwb <studioarcadacwb@gmail.com>
2020-08-20 18:34:55 -0300

    basics start

---
+++ exercicios-js/fundamendos/organizacao.js
@@ -0,0 +1,12 @@
+console.log("Sentença de código");
+
+// JavaScript é organizado em sentenças de código
+// e podem terminar com ponto e virgula ou não
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') // Padrão do curso
+    }
+
+}
---
+++ exercicios-js/fundamendos/variaveisEConstantes.js
@@ -0,0 +1,19 @@
+var a = 3
+
+// Forma mais moderna de criar váriaveis
+let b = 4
+
+var a = 30
+let b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+// Quanto mais constantes tiver no programa, melhor será para manter
+const c = 5
+// c = 50
+console.log(c)
